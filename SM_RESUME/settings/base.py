010fd9b6796523632781dbbf1195f3009bc34f3a
monkeyz <sangmin3285@gmail.com>
2021-09-13 19:26:16 +0900

    update 0913

--- SM_RESUME/settings/base.py
+++ SM_RESUME/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 from django.contrib import messages
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -24,26 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'))
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start + 1:]
-    env_list[key] = value
 
-SECRET_KEY = env_list['SECRET_KEY']
-
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    '*'  # 일단은 모든 ip가 가능하게
-]
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
